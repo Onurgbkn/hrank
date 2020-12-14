72ee26e899a662c0a00b2df6d7e5a7da920d187b
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2020-12-14 23:24:33 +0300

    s

--- Abbreviation[h].py
+++ Abbreviation[h].py
@@ -1,4 +1,4 @@
-f = open('C:/Users/lma10ur/Desktop/input/input04.txt', 'r')
+f = open('C:/Users/lma10ur/Desktop/input/input0.txt', 'r')
 
 
 
@@ -12,10 +12,17 @@ for _ in range(int(f.readline())):
         print('NO')
         continue
 
+    if [i for i in a if i.isupper()] == b:
+        print('YES')
+        continue
+
+    n = list()
+
     i = len(a)-1
     while i >= 0:
         if a[i].upper() == b[-1]:
             b.pop()
+            if a[i].islower(): n.append(a[i])
             if len(b) == 0:
                 if any([i.isupper() for i in a[:i]]):
                     break
@@ -23,7 +30,12 @@ for _ in range(int(f.readline())):
                     i = 0
 
         elif a[i].isupper():
-            break
+            if len(n) != 0 and a[i] == n[0].upper():
+                n.pop(0)
+            else:
+                print(i)
+                print(len(b))
+                break
         i -= 1
 
     if i == -1:
