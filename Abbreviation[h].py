b3cfb5fd967684098da9480f80e217b13dc451a1
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2020-12-17 16:23:50 +0300

    s

--- Abbreviation[h].py
+++ Abbreviation[h].py
@@ -1,10 +1,11 @@
-f = open('C:/Users/lma10ur/Desktop/input/input0.txt', 'r')
+f = open('C:/Users/lma10ur/Desktop/input/input12.txt', 'r')
 
 
 
 for _ in range(int(f.readline())):
     a = [_ for _ in f.readline().strip()]
     b = [_ for _ in f.readline().strip()]
+
 
     #print(''.join(a), ''.join(b))
 
@@ -22,19 +23,23 @@ for _ in range(int(f.readline())):
     while i >= 0:
         if a[i].upper() == b[-1]:
             b.pop()
+
             if a[i].islower(): n.append(a[i])
+
             if len(b) == 0:
-                if any([i.isupper() for i in a[:i]]):
-                    break
+                for i in a[:i]:
+                    if i.isupper():
+                        if n[-1].upper() == i:
+                            n.pop()
+                        else:
+                            break
                 else:
                     i = 0
 
         elif a[i].isupper():
-            if len(n) != 0 and a[i] == n[0].upper():
-                n.pop(0)
+            if len(n) != 0 and a[i] == n[-1].upper():
+                n.pop()
             else:
-                print(i)
-                print(len(b))
                 break
         i -= 1
 
--- test.py
+++ test.py
@@ -1,11 +1,35 @@
-a = 'hHhAhhcahhacaccacccahhchhcHcahaahhchhhchaachcaCchhchcaccccchhhcaahhhhcaacchccCaahhaahachhacaahhaachhhaaaCalhhchaccaAahHcchcazhachhhaaahaahhaacchAahccacahahhcHhccahaachAchahacaahcahacaahcahacaHhccccaahaahacaachcchhahhacchahhhaahcacacachhahchcaAhhcaahchHhhaacHcacahaccccaaahacCHhChchhhahhchcahaaCccccahhcaachhhacaaahcaaaccccaacaaHachaahcchaahhchhhcahahahhcaachhchacahhahahahAahaAcchahaahcaaaaahhChacahcacachacahcchHcaahchhcahaachnachhhhcachchahhhacHhCcaHhhhcaCccccaaahcahacahchahcaachcchaachahhhhhhhhcahhacacCcchahccaaaaaHhhccaAaaaCchahhccaahhacaccchhcahhcahaahhgacahcahhchcaaAccchahhhaahhccaaHcchaccacahHahChachhcaaacAhacacaacacchhchchacchchcacchachacaahachccchhhaccahcacchaccaahaaaccccccaaaaaaaHhcahcchmcHchcchaaahaccchaaachchHahcaccaaccahcacacahAhaacaacaccaccaaacahhhcacAhaCchcaacCcccachhchchcchhchahchchahchchhchcacaachahhccacachaAhaaachchhchchchhaachahaahahachhaaaccacahhcacchhhaaachaaacAahhcachchachhhcacchacaaChCahhhccahChaachhcahacchanaaacchhhccacacchcahccchAcahacaaachhacchachccaaHacaacAhahcCh'
-b = 'HAHHCHAACCCAHCHHAHHAHCACCHCCHHCAAHHCACCCAHHHACAAHHHHCHHCAHHAHHAAAHAACAAHAHHCAHAHACHACHCHACACHAAHHAAAHCAHHACACAACHHHCHAHCAHCHHHAHAHACCAAAHCHHCHHCCAACCCCAACHACAACAAHACHCHAHHACCHCAHHHAAACHACAACHCACACAHHCCHAHACCCACCAACHCHHHCCCCCHCCAHHCAAHHAHHHHHHHAACCCCAHCCAAAAAHHHAAAACCAHHCAHACACCHHCHAHAHHCHAACHHHHHCCHCCAHAHCHCAAACCACCCCHACCACHHACHHACACHACCAACCCCAAAAHHAHCHHHCCAHCCHACHHAHCCACACCHAHAAACACCCCAHCCAHACCCCCCHCCHHCHHHHCHCHCAHHHACHAHAACCCAAAACHAACAAAHHAAHAAAHACHHCACHCCHCHAACHACACHHCCCCCAHCACHAAAHCHCAHACAAC'
-n = 'CaAcaHachchaaahcAchaccccchhcacahcaahChcchCNO'
+def func(a, b):
+    if (len(a) < len(b)):
+        return False
 
-print(a[::-1])
-print(b[::-1])
-print(n)
+    if (len(b) == 0):
+        for c in a:
+            if (ord(c) < 97):
+                return False
+        return True
 
-print()
-print(a[900:])
-print(b[450:])
+    if (a[0] != b[0]):
+        if (ord(a[0]) >= 97):
+            return func(a[1:], b) or func(a[0].capitalize() + a[1:], b)
+        else:
+            return False
+    else:
+        return func(a[1:], b[1:])
+
+
+def abbreviation(a, b):
+    if (func(a, b)):
+        return "YES"
+    else:
+        return "NO"
+
+
+f = open('C:/Users/lma10ur/Desktop/input/input08.txt', 'r')
+for _ in range(int(f.readline())):
+    a = f.readline()
+    b = f.readline()
+
+    print(abbreviation(a, b))
+
+
+f.close()
